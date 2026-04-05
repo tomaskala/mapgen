c225ba3174983e005643f7e5382da4f0fe5d7c6a
Tomas Kala <me@tomaskala.com>
2026-04-05 14:01:35 +0200

    Add profiling support

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
+	"log"
 	"math/rand/v2"
+	"os"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/fogleman/gg"
 	"tomaskala.com/mapgen/field"
 	"tomaskala.com/mapgen/graph"
 	"tomaskala.com/mapgen/renderer"
 	"tomaskala.com/mapgen/streamline"
+)
+
+const (
+	exitSuccess      = 0
+	exitIOError      = 1
+)
+
+var (
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+	memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 )
 
 type config struct {
@@ -63,7 +78,21 @@ func buildGraph(width, height int, tf field.TensorField, cfg config, rng *rand.R
 	return graph.BuildGraph(width, height, cfg.dSep, majorLines, minorLines)
 }
 
-func main() {
+func run() int {
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Print("could not create CPU profile: ", err)
+			return exitIOError
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Print("could not start CPU profile: ", err)
+			return exitIOError
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	tf := field.TensorField{
 		field.Grid(field.Vector{X: 320.0, Y: 100.0}, field.Vector{X: 1.0, Y: 0.0}, 400.0),
 		field.Grid(field.Vector{X: 120.0, Y: 700.0}, field.Vector{X: 0.0, Y: -1.0}, 300.0),
@@ -144,5 +173,28 @@ func main() {
 	dc.SetLineWidth(2)
 	dc.Stroke()
 
-	dc.SavePNG(output)
+	if err := dc.SavePNG(output); err != nil {
+		return exitIOError
+	}
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Print("could not create memory profile: ", err)
+			return exitIOError
+		}
+		defer f.Close()
+		runtime.GC()
+		if err := pprof.Lookup("allocs").WriteTo(f, 0); err != nil {
+			log.Print("could not write memory profile: ", err)
+			return exitIOError
+		}
+	}
+
+	return exitSuccess
+}
+
+func main() {
+	flag.Parse()
+	os.Exit(run())
 }
