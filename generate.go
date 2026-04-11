34d9c20668937118b0d08b425a02991222e0ce2e
Tomas Kala <me@tomaskala.com>
2026-04-11 17:30:54 +0200

    Initial sampling tweaks

--- generate.go
+++ generate.go
@@ -15,8 +15,10 @@ import (
 const (
 	initialPopFraction = 0.6
 	initialPopStep     = 4
-	initialPopScale    = 0.002
+	initialPopScale    = 0.005
 	initialPopOctaves  = 3
+
+	minCentreDistScale = 0.15
 
 	minGrid = 2
 	maxGrid = 4
@@ -27,13 +29,13 @@ const (
 
 var (
 	mainRoadCfg = config.Config{
-		NumSeeds:     30,
+		NumSeeds:     20,
 		ConstDensity: true,
-		DSep:         200.0,
-		DTest:        100.0,
+		DSep:         220.0,
+		DTest:        110.0,
 		DLookahead:   300.0,
 		RkStep:       1.0,
-		MaxLength:    1200.0,
+		MaxLength:    1500.0,
 	}
 
 	majorRoadCfg = config.Config{
@@ -49,11 +51,11 @@ var (
 	minorRoadCfg = config.Config{
 		NumSeeds:     30,
 		ConstDensity: false,
-		DSep:         20.0,
-		DTest:        15.0,
+		DSep:         16.0,
+		DTest:        12.0,
 		DLookahead:   40.0,
 		RkStep:       1.0,
-		MaxLength:    800.0,
+		MaxLength:    600.0,
 	}
 )
 
@@ -110,31 +112,7 @@ func traceStreamlines(
 }
 
 func initializeTensorField(width, height int, population field.NoiseField, rng *rand.Rand) field.TensorField {
-	minPop := math.MaxFloat64
-	maxPop := -math.MaxFloat64
-
-	for y := 0; y < height; y += initialPopStep {
-		for x := 0; x < width; x += initialPopStep {
-			v := vector.Vec2{X: float64(x), Y: float64(y)}
-			density := population.Evaluate(v)
-			minPop = min(minPop, density)
-			maxPop = max(maxPop, density)
-		}
-	}
-
-	var candidates []vector.Vec2
-	for y := 0; y < height; y += initialPopStep {
-		for x := 0; x < width; x += initialPopStep {
-			candidate := vector.Vec2{X: float64(x), Y: float64(y)}
-			if population.Evaluate(candidate) > (maxPop-minPop)*initialPopFraction {
-				candidates = append(candidates, candidate)
-			}
-		}
-	}
-
-	rng.Shuffle(len(candidates), func(i, j int) {
-		candidates[i], candidates[j] = candidates[j], candidates[i]
-	})
+	candidates := samplePopulationCenters(width, height, population, rng)
 
 	makeGrid := func(p vector.Vec2) field.BasisField {
 		theta := rng.Float64() * math.Pi
@@ -171,5 +149,57 @@ func initializeTensorField(width, height int, population field.NoiseField, rng *
 		tf = append(tf, makeRadial(p))
 	}
 
+	globalGridCenter := vector.Vec2{X: float64(width) / 2.0, Y: float64(height) / 2.0}
+	globalGridDir := vector.Vec2{X: 1.0, Y: 0.0}
+	globalGridRadius := 2.0 * float64(width)
+	tf = append(tf, field.Grid(globalGridCenter, globalGridDir, globalGridRadius))
+
 	return tf
+}
+
+func samplePopulationCenters(width, height int, population field.NoiseField, rng *rand.Rand) []vector.Vec2 {
+	minPop := math.MaxFloat64
+	maxPop := -math.MaxFloat64
+
+	for y := 0; y < height; y += initialPopStep {
+		for x := 0; x < width; x += initialPopStep {
+			v := vector.Vec2{X: float64(x), Y: float64(y)}
+			density := population.Evaluate(v)
+			minPop = min(minPop, density)
+			maxPop = max(maxPop, density)
+		}
+	}
+
+	var candidates []vector.Vec2
+	for y := 0; y < height; y += initialPopStep {
+		for x := 0; x < width; x += initialPopStep {
+			candidate := vector.Vec2{X: float64(x), Y: float64(y)}
+			if population.Evaluate(candidate) > (maxPop-minPop)*initialPopFraction {
+				candidates = append(candidates, candidate)
+			}
+		}
+	}
+
+	rng.Shuffle(len(candidates), func(i, j int) {
+		candidates[i], candidates[j] = candidates[j], candidates[i]
+	})
+
+	var centers []vector.Vec2
+	minDist := minCentreDistScale * float64(width)
+	for _, c := range candidates {
+		tooClose := false
+
+		for _, center := range centers {
+			if c.Dist2(center) < minDist*minDist {
+				tooClose = true
+				break
+			}
+		}
+
+		if !tooClose {
+			centers = append(centers, c)
+		}
+	}
+
+	return centers
 }
